dfc6cded55894ee9bd43a9b9b5f170c87f9bd3a8
Jatinbhardwaj30 <bhardwajjatin727@gmail.com>
2025-07-20 13:23:23 +0530

    first commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,120 @@
+import { upsertStreamUser } from "../lib/stream.js";
+import User from "../models/User.js";
+import jwt from 'jsonwebtoken';
+
+export async function signup(req,res){
+    const { fullName, email, password } = req.body;
+    
+
+    try{
+        if(!fullName || !email || !password){
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+        const existingUser = await User.findOne({ email });
+        if(existingUser){
+            return res.status(400).json({ message: "User with this email already exists" });
+        }
+
+        const idx=Math.floor(Math.random() * 100)+1; // Random index for profile picture
+        const randomAvatar=`https://avatar.iran.liara.run/public/${idx}.png`; // Random avatar URL
+
+        const newUser = await User.create({
+            email,
+            fullName,
+            password,
+            profilePic: randomAvatar // Assigning random avatar
+        });
+        
+
+        try{
+            await upsertStreamUser({
+            id: newUser._id.toString(),
+            name: newUser.fullName,
+            image: newUser.profilePic || "",
+        });
+        console.log(`Stream user created successfully for ${newUser.fullName}`);
+
+        }catch(error){
+            console.error("Error creating Stream user:", error);    
+        }
+
+        const token=jwt.sign({ userid: newUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' });
+
+
+          res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+            sameSite :"strict", // Helps prevent CSRF attacks
+            secure : process.env.NODE_ENV === 'production' // Use secure cookies in production
+          })
+
+        res.status(201).json({
+            success: true,
+            user: newUser,
+        });
+
+    }catch(error){
+        console.error("Error in signup:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+   
+}
+
+export async function login(req,res){
+  try{
+
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.status(400).json({ message: "Email and password are required" });
+    }
+    const user =await User.findOne({ email });
+    if(!user){
+        return res.status(404).json({ message: "Invaild email or password" });
+    }
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token=jwt.sign({ userid: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' });
+          res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+            sameSite :"strict", // Helps prevent CSRF attacks
+            secure : process.env.NODE_ENV === 'production' // Use secure cookies in production
+          });
+
+    res.status(200).json({
+        success: true, 
+        user});             
+
+
+
+  }catch(error){
+      console.error("Error in login:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function logout(req,res){
+    try{
+        res.clearCookie("jwt", {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === 'production'
+        });
+        res.status(200).json({ message: "Logged out successfully" });
+    }catch(error){
+        console.error("Error in logout:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
