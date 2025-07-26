ab5767d9225d56806c172d406bff2f09bee404b5
Jatinbhardwaj30 <bhardwajjatin727@gmail.com>
2025-07-26 12:22:23 +0530

    changes

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -31,22 +31,23 @@ const FriendCard = ({ friend }) => {
     </div>
   );
 };
+
 export default FriendCard;
 
- export function getLanguageFlag(language) {
-  if (!language) return null;
+export function getLanguageFlag(language) {
+ if (!language) return null;
 
-  const langLower = language.toLowerCase();
-  const countryCode = LANGUAGE_TO_FLAG[langLower];
+ const langLower = language.toLowerCase();
+ const countryCode = LANGUAGE_TO_FLAG[langLower];
 
-  if (countryCode) {
-    return (
-      <img
-        src={`https://flagcdn.com/24x18/${countryCode}.png`}
-        alt={`${langLower} flag`}
-        className="h-3 mr-1 inline-block"
-      />
-    );
-  }
-  return null;
+ if (countryCode) {
+   return (
+     <img
+       src={`https://flagcdn.com/24x18/${countryCode}.png`}
+       alt={`${langLower} flag`}
+       className="h-3 mr-1 inline-block"
+     />
+   );
+ }
+ return null;
 }
